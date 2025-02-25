e78b39b2ee75034824259981c918aca2b01d6cfe
Adrian <adrianlucassouzadssilva@gmail.com>
2025-02-25 19:14:37 -0400

    Errata exercicio 3

--- Aula 01/exercicio3.py
+++ Aula 01/exercicio3.py
@@ -4,6 +4,8 @@ n2 = int (input("Digite o segundo número: "))
 soma = n1 + n2
 
 if soma > 20:
-    print (soma + 8)
+    print("Soma é maior que 20: ", soma)
+    print("Nova valor: ", soma + 8)
 else:
-    print (soma - 5)
+   print("Soma é menor que 20: ", soma)
+   print("Nova valor: ", soma -5)
