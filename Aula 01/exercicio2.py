c2fbfc183a6d93f661c2d75a897c9aba5bfc85c8
Adrian <adrianlucassouzadssilva@gmail.com>
2025-02-24 23:22:17 -0400

    Exercicios da aula 01

---
+++ Aula 01/exercicio2.py
@@ -0,0 +1,7 @@
+a = int (input("Digite o primeiro número: "))
+b = int (input("Digite o segundo número: "))
+
+if a < b:
+    print("O menor número é ", a)
+else:
+    print("O menor número é ", b)
---
+++ Aula 01/exercicio3.py
@@ -0,0 +1,9 @@
+n1 = int (input("Digite o primeiro número: "))
+n2 = int (input("Digite o segundo número: "))
+
+soma = n1 + n2
+
+if soma > 20:
+    print (soma + 8)
+else:
+    print (soma - 5)
